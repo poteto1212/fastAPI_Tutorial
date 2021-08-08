2ecee772c2a19cad539eab2ce9106d08852abaf2
Your Name <you@example.com>
2021-08-08 02:53:07 +0000

    データベース設計

--- db.py
+++ db.py
@@ -1,17 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
-
-#sqlite陽用のデータベース設定
-
-Base=declarative_base()
-RDB_PATH='sqlite:///db.sqlite3'
-ECHO_LOG= True
-
-
-engine=create_engine(
-    RDB_PATH,echo=ECHO_LOG
+from sqlalchemy.orm import sessionmaker
+ 
+Base = declarative_base()
+RDB_PATH = 'sqlite:///db.sqlite3'
+ECHO_LOG = True
+ 
+engine = create_engine(
+   RDB_PATH, echo=ECHO_LOG
 )
-
-Session=sessionmaker(bind=engine)
-session=Session()
+ 
+Session = sessionmaker(bind=engine)
+session = Session()
